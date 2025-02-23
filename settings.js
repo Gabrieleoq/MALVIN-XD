1435947b3acce8b2ab76e6c6706e1f5cc7abc413
Gabrieleoq <givanovcostin@gmail.com>
2025-02-23 08:17:27 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~8xIlEaKR#z2wnCw17vnG2GEj5RKjoRb94MhPPGGKNCiS5thLkpek",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~x8p3yLbI#Rk83tGP_fkz-C10BScp1QXppmo80ShGOWotPzplEKkA",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
 
-BOT_NAME: process.env.BOT_NAME || "✦ᴍᴀʟᴠɪɴ xᴅ v2",
+BOT_NAME: process.env.BOT_NAME || "Gigel v1.2",
 // add bot name here for menu
 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
@@ -20,7 +20,7 @@ CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 STICKER_NAME: process.env.STICKER_NAME || "MALVIN-XD",
 // type sticker pack name  
 
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
+CUSTOM_REACT_EMOJIS: "👻,💀,👹,👺,👿,🥱,🥵,🤣,🧠,🐵,🐶",
 // chose custom react emojis by yourself 
 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
@@ -29,7 +29,7 @@ DELETE_LINKS: process.env.DELETE_LINKS || "true",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "400771048185",
 // add your bot owner number
 
-OWNER_NAME: process.env.OWNER_NAME || "✦ᴍᴀʟᴠɪɴ ᴛᴇᴄʜ🪀✦",
+OWNER_NAME: process.env.OWNER_NAME || "Gigel v1.2",
 // add bot owner name
 
 DESCRIPTION: process.env.DESCRIPTION || "> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ✦ɴᴇxᴜs ᴛᴇᴄʜ✦ ɪɴᴄ*",
